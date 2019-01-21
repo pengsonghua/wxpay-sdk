78e1d1f1f0c9a2d0f01f21ac13745a38261fe5c1
pengsonghua <peng1songhua@163.com>
2019-01-21 14:47:20 +0800

    初始化

---
+++ src/main/java/com/github/wxpay/sdk/WxpaySdkApplication.java
@@ -0,0 +1,14 @@
+package com.github.wxpay.sdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxpaySdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxpaySdkApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/github/wxpay/sdk/WxpaySdkApplicationTests.java
@@ -0,0 +1,17 @@
+package com.github.wxpay.sdk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WxpaySdkApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
